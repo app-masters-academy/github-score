f0468461b1cb1c195cb2be29f3dabe919d7e61ec
Ariam Moresco Bartsch <ambartsch@inf.ufpel.edu.br>
2021-08-27 16:44:11 -0300

    commit with a working code, but a dumb calculation

---
+++ src/controller.js
@@ -0,0 +1,42 @@
+const { Octokit } = require("@octokit/rest")
+
+const octokit = new Octokit()
+
+module.exports = {
+    getScore: async function(req, res){
+    const repoScore = []
+    let totalScore = 0
+    const user =  await octokit.rest.repos.listForUser({
+        username: req.params.username
+    }).then( (data) => {
+        let score = 0
+        for(let i = 0; i < data.data.length; i++){
+            score = 0
+            score += data.data[i].stargazers_count
+            score += data.data[i].watchers_count
+            score = score - data.data[i].open_issues_count
+            score = score * ( 1 + data.data[i].forks_count / 100)
+
+            totalScore += score
+            repoScore.push({name: data.data[i].name, score: score })
+            
+        }
+        const json = {
+            repos: repoScore,
+            totalScore: totalScore
+        }
+        console.log(json)
+        res.send(json)
+    })
+        /*
+
+    JSON.parse(user)
+    //const repos = user.data
+    let score
+    score = user.data[0].stargazers_count
+    let score = 0
+    */
+    res.send(user)
+
+    }
+}
